71fa3087b2efaa6f9549b7bef5d72f1a1c0f2c89
Jesse Rowley <rowleyj@mcmaster.ca>
2021-12-06 09:31:40 -0500

    add distance calculation functions and test

---
+++ src/lib/gps.js
@@ -0,0 +1,37 @@
+const earthRadius = 6378.8; // in kms
+
+/**
+ * Converts degress to radians
+ * @param {Number} deg 
+ * @returns {Number} rad
+ */
+function deg2Rad (deg) {
+	return deg / (180 / Math.PI);
+}
+
+function getDistanceBetweenPoints (lat1, lon1, lat2, lon2) {
+	let innerVal = (Math.sin(lat1) * Math.sin(lat2)) + Math.cos(lat1) * Math.cos(lat2) * Math.cos(lon2 - lon1)
+	return earthRadius * Math.acos(innerVal);
+}
+
+
+/**
+ * Calculates the distance covered by a list of gps coordinates
+ * @param {[[Number,Number]]} coordArray - lat, lon
+ */
+function gpsCoordsToDistance (coordArray) {
+	if (!coordArray) return 0;
+	let distance = 0;
+
+	// slide over values and sum distances
+	for (let i = 0; i < coordArray.length - 1; i++) {
+		let [lat1, lon1] = coordArray[i];
+		let [lat2, lon2] = coordArray[i + 1];
+		distance = distance + getDistanceBetweenPoints(deg2Rad(lat1), deg2Rad(lon1), deg2Rad(lat2), deg2Rad(lon2));
+	}
+
+	return distance;
+}
+
+
+module.exports.gpsCoordsToDistance = gpsCoordsToDistance;
---
+++ test/unit/lib/gps.test.js
@@ -0,0 +1,15 @@
+const { expect } = require("chai");
+const { gpsCoordsToDistance } = require("../../../src/lib/gps");
+
+
+describe('GPS Lib', () => {
+	describe('Converting lat lon array to distance', () => {
+
+		it('should properly calculate distance between a set of points', () => {
+			let coordArray = [[53.32055, -1.72972], [53.31861, -1.69972]]
+
+			expect(gpsCoordsToDistance(coordArray)).to.be.equal(2.006768618764435);
+			expect(gpsCoordsToDistance(coordArray)).to.exist
+		});
+	});
+});
